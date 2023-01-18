1ecc13877b45a7d9eacb56c1a7586228c55d5a8c
Vladyslav8919 <vladyslav.pshevlotskyi@gmail.com>
2023-01-18 09:20:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions And Answers About Login</h3>
+        <section className="info">
+          {data.map((item) => (
+          <SingleQuestion key={item.id} {...item} />
+        ))}
+        </section>
+        
+      </div>
+    </main>
+    
+  );
+}
+
+export default App;
